0c6841e07b2657361e5a07c62a68d4000dede6d7
d-velev <dimitar.velev@quanterall.com>
2019-08-02 09:42:16 +0300

    WIP

--- frontend/contract_monitor/src/App.vue
+++ frontend/contract_monitor/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <ae-input-plain placeholder="Contract address" v-model="contractAddress" />
     <ae-button face="round" fill="primary" @click="submit">Hello World</ae-button>
-    <div id="chart"><line-chart :chart-data="datacollection"></line-chart></div>
+    <div id="chart"><line-chart :chartData="datacollection"/></div>
   </div>
 </template>
 
@@ -13,6 +13,7 @@ import LineChart from './LineChart.js'
 
 let phoenix = require('phoenix-js')
 let axios = require('axios');
+let moment = require('moment')
 
 export default {
   name: 'app',
@@ -45,25 +46,39 @@ export default {
 
   },
   methods: {
+    newDate(days) {
+      return moment().add(days, 'd');
+    },
     submit() {
       axios.get(`http://localhost:4000/set-contract/${this.contractAddress}/edit`)
       axios.get(`https://testnet.mdw.aepps.com/middleware/contracts/transactions/address/${this.contractAddress}`).then(function(res){
-        res.data
+        let data = {}
+        for(i = 0; i < res.data.transactions.length; i++){
+          
+        }
+        this.fillData(res.data)
       })
     },
-    fillData () {
-        this.datacollection = {
-          labels: [1,2,3],
-          datasets: [{data: [1,3,2]}]
-        }
+    fillData (data) {
+      this.datacollection = {
+        labels: [this.newDate(1),this.newDate(6),this.newDate(7)],
+        datasets: [{
+           data: [1,2,3],
+           label: "bla",
+           borderWidth: 2,
+           borderColor: "#3e95cd",
+           fill: false,
+           pointRadius: 0
+        }]
       }
+    }
   }
 }
 </script>
 
 <style>
 #chart{
-  width: 400px;
+  width: 50%;
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
